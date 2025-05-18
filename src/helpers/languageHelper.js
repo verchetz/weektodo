13cec5f5117095514602787bc105273a91186643
ShaKend <shaunkennt@gmail.com>
2025-05-18 20:37:38 +0700

    refactor languageHelper: bloaters, long switch statement

--- src/helpers/languageHelper.js
+++ src/helpers/languageHelper.js
@@ -1,64 +1,28 @@
 import { es, enUS, fr, pt, ru, zhCN, de, it, ar, pl, zhTW, ja, tr, uk, ko, hi, vi, he } from "date-fns/locale";
 
+const localeMap = {
+  es,
+  en: enUS,
+  fr,
+  pt,
+  it,
+  ar,
+  pl,
+  ru,
+  zh_cn: zhCN,
+  zh_tw: zhTW,
+  de,
+  ja,
+  tr,
+  uk,
+  ko,
+  hi,
+  vi,
+  he,
+};
+
 export default {
   getLanguagePack(lang) {
-    let return_lang = null;
-    switch (lang) {
-      case "es":
-        return_lang = es;
-        break;
-      case "en":
-        return_lang = enUS;
-        break;
-      case "fr":
-        return_lang = fr;
-        break;
-      case "pt":
-        return_lang = pt;
-        break;
-      case "it":
-        return_lang = it;
-        break;
-      case "ar":
-        return_lang = ar;
-        break;
-      case "pl":
-        return_lang = pl;
-        break;
-      case "ru":
-        return_lang = ru;
-        break;
-      case "zh_cn":
-        return_lang = zhCN;
-        break;
-      case "zh_tw":
-        return_lang = zhTW;
-        break;
-      case "de":
-        return_lang = de;
-        break;
-      case "ja":
-        return_lang = ja;
-        break;
-      case "tr":
-        return_lang = tr;
-        break;
-      case "uk":
-        return_lang = uk;
-        break;
-      case "ko":
-        return_lang = ko;
-        break;
-      case "hi":
-        return_lang = hi;
-        break;
-      case "vi":
-        return_lang = vi;
-        break;
-      case "he":
-        return_lang = he;
-        break;
-    }
-    return return_lang;
+    return localeMap[lang] || null;
   },
 };
